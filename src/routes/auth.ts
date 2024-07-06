a9c5d749214c725a1a68b981caa6ba5ad18bc47f
Mujtaba-Abbasi <gmabbasi92@gmail.com>
2024-07-06 23:14:08 +0500

    Define the API endpoint for register

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,9 +1,11 @@
 import { Router } from "express";
-import { login, logout } from "../controllers";
+import { login, logout, register } from "../controllers";
 import { validate } from "../middlewares";
 import { LoginSchema } from "../utils";
 
 export const authRouter = Router();
 
 authRouter.post("/login", validate(LoginSchema), login);
+authRouter.post("/register", register);
+
 authRouter.get("/logout", logout);
