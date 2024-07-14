4e7c4850d65dc9f1c4949aee4119acbfb1818204
Mujtaba-Abbasi <gmabbasi92@gmail.com>
2024-07-14 23:25:23 +0500

    Create endpoint for fetching user's treats

--- src/controllers/treat.ts
+++ src/controllers/treat.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from "express";
 import { pool } from "../db";
 import { USER_ROLE } from "../constants";
+import { UpdateTreatSchemaType } from "../utils/types";
 
 export const createTreat = async (request: Request, response: Response) => {
   try {
@@ -32,11 +33,6 @@ export const createTreat = async (request: Request, response: Response) => {
       message: error?.message || "Internal server error",
     });
   }
-};
-
-export const updateTreat = async (request: Request, response: Response) => {
-  try {
-  } catch (error) {}
 };
 
 export const deleteTreat = async (request: Request, response: Response) => {
@@ -81,6 +77,97 @@ export const deleteTreat = async (request: Request, response: Response) => {
   } catch (error: any) {
     response.status(500).json({
       message: error?.message || "Something went wrong deleting the treat",
+    });
+  }
+};
+
+export const getAllTreats = async (request: Request, response: Response) => {
+  try {
+    const { user } = request.locals;
+    const { id } = request.params;
+
+    const userId = id ?? user.id;
+
+    const userExists = await pool.query(`SELECT id FROM "user" WHERE id = $1`, [
+      userId,
+    ]);
+
+    if (!userExists.rows[0]) {
+      return response.status(400).json({
+        message: "User not found",
+      });
+    }
+
+    const getAllTreatsQuery = `
+      SELECT t.id, t.title, t.description, t.user_id AS "userId", t.created_at AS "createdAt", t.is_fulfilled AS "isFulfilled", t.fulfilled_on AS "fulfilledOn"
+      FROM treat t
+      LEFT JOIN "user" u ON t.user_id = u.id
+      WHERE t.user_id = $1
+      ORDER BY t.created_at DESC
+    `;
+
+    const getAllTreatsRes = await pool.query(getAllTreatsQuery, [userId]);
+
+    const userTreats = getAllTreatsRes.rows;
+
+    response.status(200).json({
+      message: "Successfully fetched the user treats",
+      data: userTreats,
+    });
+  } catch (error: any) {
+    response.status(400).json({
+      message: error?.message || "Something went wrong fetching user's treats",
+    });
+  }
+};
+
+export const updateTreat = async (request: Request, response: Response) => {
+  try {
+    const { user } = request.locals;
+    const { id, title, description }: UpdateTreatSchemaType = request.body;
+
+    const fetchQuery = `
+        SELECT id, user_id as "userId"
+        FROM treat
+        WHERE id = $1
+    `;
+
+    const fetchQueryRes = await pool.query(fetchQuery, [id]);
+    const treat = fetchQueryRes.rows[0];
+
+    if (!treat) {
+      return response.status(400).json({
+        message: "Treat not found with the provided id",
+      });
+    }
+
+    if (treat.userId !== user.id && user.role !== USER_ROLE.Admin) {
+      return response.status(400).json({
+        message: "You are not authorized to edit this treat",
+      });
+    }
+
+    const updateQuery = `
+      UPDATE treat
+      SET title = $1, description = $2
+      WHERE id = $3
+      RETURNING Id, title, description
+    `;
+
+    const updateQueryRes = await pool.query(updateQuery, [
+      title,
+      description,
+      id,
+    ]);
+
+    response.status(200).json({
+      message: "Treat has been updated successfully",
+      data: updateQueryRes.rows[0],
+    });
+  } catch (error: any) {
+    console.log("The error in updating treat =>", error);
+    response.status(400).json({
+      message: error?.message || "Something went wrong updating the treat",
     });
   }
 };
